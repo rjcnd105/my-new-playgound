00fc0679d3cbd7ec566acf217344092aae469d35
김회준 <rjcnd123@gmail.com>
2023-11-16 11:34:59 +0900

    fix: simplify

--- packages/hj_fp-ts-lib/src/lib/schema/utilityTypes.ts
+++ packages/hj_fp-ts-lib/src/lib/schema/utilityTypes.ts
@@ -1,5 +1,6 @@
 import type { Schema } from "@effect/schema";
 import type { FromOptionalKeys } from "@effect/schema/Schema";
+import type Types = require("effect/Types");
 
 type AllRecord = Record<PropertyKey, any>;
 
@@ -16,22 +17,28 @@ export type AllSchemaKeys<Fields> =
   | FromOptionalKeys<Fields>;
 
 // TODO: 추후 ApiSpecFetcher에서 이 타입 유틸들로 대체
-export type FromAllStruct<Fields extends AllRecord> = {
-  readonly [K in Extract<keyof Fields, SchemaKeys<Fields>>]: Schema.Schema.From<
-    Fields[K]
-  >;
-} & {
-  readonly [K in FromOptionalKeys<Fields>]?: Schema.Schema.From<Fields[K]>;
-} & {
-  readonly [K in Exclude<keyof Fields, AllSchemaKeys<Fields>>]: Fields[K];
-};
+export type FromAllStruct<Fields extends AllRecord> = Types.Simplify<
+  {
+    readonly [K in Extract<
+      keyof Fields,
+      SchemaKeys<Fields>
+    >]: Schema.Schema.From<Fields[K]>;
+  } & {
+    readonly [K in FromOptionalKeys<Fields>]?: Schema.Schema.From<Fields[K]>;
+  } & {
+    readonly [K in Exclude<keyof Fields, AllSchemaKeys<Fields>>]: Fields[K];
+  }
+>;
 
-export type ToAllStruct<Fields extends AllRecord> = {
-  readonly [K in Extract<keyof Fields, SchemaKeys<Fields>>]: Schema.Schema.From<
-    Fields[K]
-  >;
-} & {
-  readonly [K in FromOptionalKeys<Fields>]?: Schema.Schema.From<Fields[K]>;
-} & {
-  readonly [K in Exclude<keyof Fields, AllSchemaKeys<Fields>>]: Fields[K];
-};
+export type ToAllStruct<Fields extends AllRecord> = Types.Simplify<
+  {
+    readonly [K in Extract<
+      keyof Fields,
+      SchemaKeys<Fields>
+    >]: Schema.Schema.From<Fields[K]>;
+  } & {
+    readonly [K in FromOptionalKeys<Fields>]?: Schema.Schema.From<Fields[K]>;
+  } & {
+    readonly [K in Exclude<keyof Fields, AllSchemaKeys<Fields>>]: Fields[K];
+  }
+>;
